4c2f1239a49d4ee72c02b2f97eca7ab67241311b
Nishant garg <iamnishantgarg@gmail.com>
2020-04-29 20:22:57 +0530

    shouldComponentUpddate added to leaderboard

--- src/components/Leaderboard/Leaderboard.js
+++ src/components/Leaderboard/Leaderboard.js
@@ -12,41 +12,57 @@ import Player from "../Player/Player";
 //   console.log(res.body);
 // };
 
-export default function Leaderboard() {
-  const players = [
-    { Teamno: 1, username: "NishantA", kills: 43, rank: 1 },
-    { Teamno: 221, username: "NishantB", kills: 42, rank: 2 },
-    { Teamno: 211, username: "NishantC", kills: 33, rank: 23 },
-    { Teamno: 12, username: "NishantD", kills: 13, rank: 22 },
-    { Teamno: 1, username: "NishantA", kills: 43, rank: 1 },
-    { Teamno: 221, username: "NishantB", kills: 42, rank: 2 },
-    { Teamno: 211, username: "NishantC", kills: 33, rank: 23 },
-    { Teamno: 12, username: "NishantD", kills: 13, rank: 22 },
-    { Teamno: 1, username: "NishantA", kills: 43, rank: 1 },
-    { Teamno: 221, username: "NishantB", kills: 42, rank: 2 },
-    { Teamno: 211, username: "NishantC", kills: 33, rank: 23 },
-    { Teamno: 12, username: "NishantD", kills: 13, rank: 22 },
-  ];
-  // console.log(getPlayers());
-  // getPlayers();
+export default class Leaderboard extends React.Component {
+  state = {
+    players: [
+      { Teamno: 1, username: "NishantA", kills: 43, rank: 1 },
+      { Teamno: 221, username: "NishantB", kills: 42, rank: 2 },
+      { Teamno: 211, username: "NishantC", kills: 33, rank: 23 },
+      { Teamno: 12, username: "NishantD", kills: 13, rank: 22 },
+      { Teamno: 1, username: "NishantA", kills: 43, rank: 1 },
+      { Teamno: 221, username: "NishantB", kills: 42, rank: 2 },
+      { Teamno: 211, username: "NishantC", kills: 33, rank: 23 },
+      { Teamno: 12, username: "NishantD", kills: 13, rank: 22 },
+      { Teamno: 1, username: "NishantA", kills: 43, rank: 1 },
+      { Teamno: 221, username: "NishantB", kills: 42, rank: 2 },
+      { Teamno: 211, username: "NishantC", kills: 33, rank: 23 },
+      { Teamno: 12, username: "NishantD", kills: 13, rank: 22 },
+    ],
+  };
 
-  const playerRows = players.map((play, ind) => {
-    return <Player player={play} key={ind} />;
-  });
-  return (
-    <div className={classes.Leaderboard}>
-      <table>
-        <thead>
-          <tr>
-            <th>Team No.</th>
-            <th>Username</th>
-            <th>Kills</th>
-            <th>Rank</th>
-          </tr>
-        </thead>
+  shouldComponentUpdate(newProps, newStates) {
+    return this.state.players !== newStates.players;
+  }
 
-        <tbody>{playerRows}</tbody>
-      </table>
-    </div>
-  );
+  render() {
+    const playerRows = this.state.players.map((play, ind) => {
+      return <Player player={play} key={ind} />;
+    });
+
+    return (
+      <div className={classes.Leaderboard}>
+        <table>
+          <thead>
+            <tr>
+              <th>Team No.</th>
+              <th>Username</th>
+              <th>Kills</th>
+              <th>Rank</th>
+            </tr>
+          </thead>
+
+          <tbody>{playerRows}</tbody>
+        </table>
+      </div>
+    );
+  }
 }
+
+// export default function Leaderboard() {
+//   // console.log(getPlayers());
+//   // getPlayers();
+
+//   const playerRows = players.map((play, ind) => {
+//     return <Player player={play} key={ind} />;
+//   });
+// }
